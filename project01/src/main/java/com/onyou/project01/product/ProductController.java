10654688c91d0b213576abf643ff53994d816846
Jun CHOI <cjonyou@naver.com>
2025-01-14 02:10:15 +0000

    수정

--- project01/src/main/java/com/onyou/project01/product/ProductController.java
+++ project01/src/main/java/com/onyou/project01/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.onyou.project01.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ProductController {
     // 사용자(화면) 요청을 던지면
     // 그에 맞는 로직을 수행할 수 있도록 서비스한테 시킬거예요
-
+    // autowired
+    @Autowired
+    private ProductService productService;
     // 상품 조회
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String findProduct(){
-        return "NoteBook";
+
+        return productService.findProduct();
     }
+
+
 
 }
--- project01/src/main/java/com/onyou/project01/product/ProductService.java
+++ project01/src/main/java/com/onyou/project01/product/ProductService.java
@@ -1,5 +1,11 @@
 package com.onyou.project01.product;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    public String findProduct(){
+        return "NoteBook-P";
+    }
     
 }
