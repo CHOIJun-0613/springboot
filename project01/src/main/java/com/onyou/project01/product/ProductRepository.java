81a398ac8bc15bdd603b0f5b7d69fe8f907a6b28
Jun CHOI <cjonyou@naver.com>
2025-01-14 02:25:45 +0000

    수정

--- project01/src/main/java/com/onyou/project01/product/ProductRepository.java
+++ project01/src/main/java/com/onyou/project01/product/ProductRepository.java
@@ -1,5 +1,16 @@
 package com.onyou.project01.product;
 
-public class ProductRepository {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+    
+    private Map<Integer, String> db = new HashMap<>();
+    private int id = 1;
+    public String findProduct(){
+        return "NoteBook-M";
+    }
 }
--- project01/src/main/java/com/onyou/project01/product/ProductService.java
+++ project01/src/main/java/com/onyou/project01/product/ProductService.java
@@ -1,11 +1,17 @@
 package com.onyou.project01.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
     public String findProduct(){
-        return "NoteBook-P";
+        //return "NoteBook-P";
+        return  productRepository.findProduct();
+
     }
     
 }
