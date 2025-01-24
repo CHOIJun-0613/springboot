81e1661eb18b182f0895d3046e895b6e0a115978
Jun CHOI <cjonyou@naver.com>
2025-01-24 15:59:52 +0900

    수정

--- init.sql
+++ init.sql
@@ -2,9 +2,9 @@ CREATE USER IF NOT EXISTS 'springboot'@'%' IDENTIFIED BY 'springboot';
 
 GRANT ALL PRIVILEGES ON *.* TO 'springboot'@'%';
 
-CREATE DATABASE IF NOT EXISTS springboot;
+CREATE DATABASE IF NOT EXISTS springbootDB;
 
-USE springboot;
+USE springbootDB;
 CREATE TABLE IF NOT EXISTS sample (
   id INT AUTOINCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
