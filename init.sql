8d5fc6adce1b127fcc2dd221b25dcfaf62070f9f
Jun CHOI <cjonyou@naver.com>
2025-01-24 16:17:15 +0900

    수정

--- init.sql
+++ init.sql
@@ -6,7 +6,11 @@ CREATE DATABASE IF NOT EXISTS springbootDB;
 
 USE springbootDB;
 CREATE TABLE IF NOT EXISTS sample (
-  id INT AUTOINCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-);
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    name VARCHAR(255)
+) COMMENT 'Sample';
+
+INSERT sample(name) VALUES('initdata');
+
 
