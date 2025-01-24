1a2aab13e3eae8c1b699cfc82a30d1c46a9e6910
Jun CHOI <cjonyou@naver.com>
2025-01-24 15:53:55 +0900

    추가

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE USER IF NOT EXISTS 'springboot'@'%' IDENTIFIED BY 'springboot';
+
+GRANT ALL PRIVILEGES ON *.* TO 'springboot'@'%';
+
+CREATE DATABASE IF NOT EXISTS springboot;
+
+USE springboot;
+CREATE TABLE IF NOT EXISTS sample (
+  id INT AUTOINCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
