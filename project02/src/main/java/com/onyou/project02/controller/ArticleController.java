7ff6bfd28bfb4a00285759975479cfdf0f975f52
Jun CHOI <cjonyou@naver.com>
2025-02-11 17:40:00 +0900

    추가수정

--- project02/src/main/java/com/onyou/project02/controller/ArticleController.java
+++ project02/src/main/java/com/onyou/project02/controller/ArticleController.java
@@ -1,14 +1,18 @@
 package com.onyou.project02.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.onyou.project02.dto.ArticleForm;
 import com.onyou.project02.entity.Article;
 import com.onyou.project02.repository.ArticleRepository;
 
+import org.springframework.ui.Model;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +42,16 @@ public class ArticleController {
         return "";
     }
     
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        // 1. id를 조회해서 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
     
 
 }
