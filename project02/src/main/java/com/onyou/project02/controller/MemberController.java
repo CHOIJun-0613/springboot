3c8c8b2ddbc083f0fb8e6be0fdfe3439c3da108c
Jun CHOI <cjonyou@naver.com>
2025-02-12 11:37:34 +0900

    수정

--- project02/src/main/java/com/onyou/project02/controller/MemberController.java
+++ project02/src/main/java/com/onyou/project02/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.onyou.project02.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import com.onyou.project02.dto.MemberForm;
 import com.onyou.project02.entity.Member;
@@ -10,6 +13,7 @@ import com.onyou.project02.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +43,26 @@ public class MemberController {
         log.info(saved.toString());
         return "";
     }
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        // 1. id를 조회해서 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+    @GetMapping("/members")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("members", memberEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "members/index";
+    }
+    
     
     
 
